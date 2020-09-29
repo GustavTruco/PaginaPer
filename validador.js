5592027a43a034a8f03dc0f69625e5ff9c982918
GustavTruco <gustavo.varas@ug.uchile.cl>
2020-09-29 10:45:45 -0300

    Ejercicio 2

---
+++ validador.js
@@ -0,0 +1,46 @@
+/**
+ * Imprime error
+ * @param {string|number} msg
+ */
+function mostrarError(msg) {
+
+    let contenedor = document.getElementById('error');
+
+    contenedor.innerHTML = msg;
+    contenedor.style.display = 'block';
+    contenedor.style.fontWeight = '800';
+
+}
+
+/**
+ Validacion del formulario
+ */
+
+
+function validacionFormulario() {
+    var mensaje = "";
+    /** @type {string}*/ let nombre = document.getElementById('nombre').value;
+
+    let regex = /^[a-zA-Z ]*$/;
+    if (nombre.length < 10 || nombre.length > 100 || !regex.test(nombre)) {
+        mensaje += "Error en el nombre del archivo\n";
+        
+    }
+    if (document.getElementById('archivo').files.length==0){
+        mensaje += "No has seleccionado un Archivo \n";
+    }
+    /*
+    La idea sería ir añadiendo más validaciones aquí, e ir concatenando el mensaje
+    de error si es que existe, para al final realizar "mostrarError(mensaje_error_concatenado)".
+     */
+    if (mensaje.length < 1){
+        return true;
+    }
+
+    mensaje=+ "Se han encontrado los siguientes errores:\n";
+    mostrarError(mensaje)
+    return False;
+
+}
+
+console.log('app v1.0'); // stack trace
