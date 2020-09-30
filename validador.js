c39fdfa03f644414df600eac61f4a239721d2ccc
GustavTruco <gustavo.varas@ug.uchile.cl>
2020-09-30 19:13:23 -0300

    Ej 2

--- validador.js
+++ validador.js
@@ -8,8 +8,11 @@ function mostrarError(msg) {
 
     contenedor.innerHTML = msg;
     contenedor.style.display = 'block';
-    contenedor.style.fontWeight = '800';
+}
 
+function eraseErrors(){
+    let contenedor = document.getElementById('error');
+    contenedor.style.display= 'None';
 }
 
 /**
@@ -23,16 +26,50 @@ function validacionFormulario() {
 
     let regex = /^[a-zA-Z ]*$/;
     if (nombre.length < 10 || nombre.length > 100 || !regex.test(nombre)) {
-        mensaje += "Error en el nombre del archivo";
+        mensaje +="<br>";
+        mensaje += "- Error en el nombre del archivo";
+        
         
     }
     if (document.getElementById('archivo').files.length==0){
-        mensaje += "No has seleccionado un Archivo";
+        mensaje +="<br>";
+        mensaje += "- No has seleccionado un Archivo";
+        
     }
-    /*
-    La idea sería ir añadiendo más validaciones aquí, e ir concatenando el mensaje
-    de error si es que existe, para al final realizar "mostrarError(mensaje_error_concatenado)".
-     */
+
+    if (document.getElementById('privacidad').value==""){
+        mensaje+="<br>"
+        mensaje+="- No ha seleccionado la privacidad del archivo"
+    }
+
+    if (document.getElementById('privacidad').value=="2"){
+        let pass=document.getElementById('password').value;
+        var notpass = ["password", "1234", "pass", "user","hackbox"];
+        if (pass==""){
+            mensaje+="<br>";
+            mensaje+="- Por favor ingrese contraseña";
+        }
+        if (notpass.includes(pass)){
+            mensaje+="<br>";
+            mensaje+="- Por favor ingrese una contraseña mas creativa";
+        }
+        if (pass.length>10){
+            mensaje+="<br>";
+            mensaje+="- Su contraseña excede el maximo de caracteres";
+        }
+    }
+
+    let autodes=document.getElementById("autodestruir").value;
+    if (autodes!="" && (autodes<1 || autodes>3153600 || isNaN(autodes))){
+        mensaje+="<br>";
+        mensaje+="- Ingrese un tiempo de autodestrucción válido";
+    }
+
+    if (document.getElementById("comentario").value.length>1000){
+        mensaje+="<br>";
+        mensaje+="- Su comentario es muy largo, sea mas breve";
+    }
+
     if (mensaje.length < 1){
         return true;
     }
