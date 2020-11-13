53805ebe5088d4b1786418cf822e52347fe5914b
GustavTruco <gustavo.varas@ug.uchile.cl>
2020-11-13 13:40:22 -0300

    Tarea 2 Inicio subir datos

--- cgi-bin/Tarea_2/basedatos.py
+++ cgi-bin/Tarea_2/basedatos.py
@@ -213,7 +213,7 @@ if mensaje=="":
         #-----------#
         #fotos
         c=0
-        print(allarchivos)
+        print(allnombres_archivos)
         if type(allarchivos[i])==list:
             for archivo in allarchivos[i]:
                 new_name=str(id_dom)+"-"+str(i)+"-"+str(c)+".png"
