38bfb10f9d50b325024336b9c26dfeaafea82c95
GustavTruco <gustavo.varas@ug.uchile.cl>
2020-11-13 20:10:04 -0300

    Working Ingreso de Datos

--- cgi-bin/Tarea_2/basedatos.py
+++ cgi-bin/Tarea_2/basedatos.py
@@ -237,18 +237,22 @@ if mensaje=="":
         #-----------#
         #fotos
         c=0
-        new_n=str(id_com)+"-"+str(id_dom)+"-"+str(i)+"-"+str(c)+".png"  
+          
         if (len(allnombres_archivos[i])>1):
             for archivo in allarchivos[i]:
+                new_n=str(id_com)+"-"+str(id_dom)+"-"+str(i)+"-"+str(c)+".png"
                 f=open("./DBIMG/"+new_n,"wb")
                 f.write(archivo.file.read())
                 f.close
+                print("FILE SAVED")
                 c+=1
         else:
+            new_n=str(id_com)+"-"+str(id_dom)+"-"+str(i)+"-"+str(c)+".png"
             archivo=allarchivos[i]
             f=open("./DBIMG/"+new_n,"wb")
             f.write(archivo.file.read())
             f.close
+            print("FILE SAVED")
             c+=1
     
         #-----------#
