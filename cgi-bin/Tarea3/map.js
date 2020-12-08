c8cadcf52fc5bc298a32ddd1fc2d738661aa5a8b
GustavTruco <gustavo.varas@ug.uchile.cl>
2020-12-08 13:19:10 -0300

    markers

--- cgi-bin/Tarea3/map.js
+++ cgi-bin/Tarea3/map.js
@@ -1443,7 +1443,7 @@ function onMarkerClick(e, comuna) {
     xhr.onerror = () => {
         errorMessage('Error en la conexión con el servidor. Esto puede causar que los gráficos no se vean. Porfavor vuelva a intentarlo mas tarde');
     }
-    xhr.open('POST', './util/maps.py');
+    xhr.open('POST', 'map.py');
     xhr.onload = (data) => {
         let datatext = data.currentTarget.responseText;
         if (xhr.status != 200 || datatext.includes('ERROR')) {
